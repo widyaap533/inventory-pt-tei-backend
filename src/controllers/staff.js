6ab9388d4a3d3ac7d1ce28179c7dde758fa9b54f
Widya Ayunindya Poge <widyaayunindyapoge@gmail.com>
2026-07-09 06:44:00 +0800

    feat: add update and get all endpoints for staff controller

--- src/controllers/staff.js
+++ src/controllers/staff.js
@@ -2,6 +2,35 @@ const prisma = require('../config/prisma');
 const { hashPassword } = require('../utils/hash');
 
 module.exports = {
+  getAllStaff: async (req, res) => {
+    try {
+      const staffList = await prisma.staff.findMany({
+        where: { is_delete: false },
+        select: {
+          id: true,
+          id_kartu: true,
+          username: true,
+          name: true,
+          position: true,
+          created_at: true,
+          updated_at: true
+        }
+      });
+
+      if (staffList.length === 0) {
+        return res.status(404).json({ status: 'error', message: 'Daftar staff tidak ditemukan' });
+      }
+
+      return res.status(200).json({
+        status: 'success',
+        data: staffList
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+    }
+  },
+
   createStaff: async (req, res) => {
     try {
       const { username, password, name, position, id_kartu } = req.body;
@@ -69,6 +98,57 @@ module.exports = {
     } catch (error) {
       console.error(error);
       return res.status(500).json({ status: 'error', message: 'Gagal mengambil data staff' });
+    }
+  },
+
+  updateStaff: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { username, id_kartu, name, position  } = req.body;
+
+      const existingStaff = await prisma.staff.findFirst({
+        where: { id: id, is_delete: false }
+      });
+
+      if (existingStaff === null) {
+        return res.status(404).json({ status: 'error', message: 'staff tidak ditemukan' });
+      }
+      if (username && username !== existingStaff.username) {
+        const checkUsername = await prisma.staff.findUnique({ where: { username: username } });
+        if (checkUsername) {
+          return res.status(400).json({ status: 'error', message: 'Username sudah digunakan oleh staff lain' });
+        }
+      }
+      if (id_kartu && id_kartu !== existingStaff.id_kartu) {
+        const checkKartu = await prisma.staff.findUnique({ where: { id_kartu: id_kartu } });
+        if (checkKartu) {
+          return res.status(400).json({ status: 'error', message: 'ID Kartu sudah terdaftar pada staff lain' });
+        }
+      }
+
+      const updateData = {
+        id: existingStaff.id,
+        username: username,
+        id_kartu: id_kartu,
+        name: name,
+        position: position
+      };
+      const updatedStaff = await prisma.staff.update({
+        where: { id: existingStaff.id },
+        data: updateData
+      });
+
+      return res.status(200).json({
+        status: 'success',
+        message: 'Data device berhasil diupdate',
+        data: {
+          id: updateData.id,
+          name: updateData.username
+        }
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ status: 'error', message: 'Gagal mengupdate staff' });
     }
   },
 
